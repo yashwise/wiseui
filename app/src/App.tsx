71254e06e637e17166586f7aad1cd06339e02180
Yashvardhan Nevatia <yashvardhannevatia@Yashvardhans-MacBook-Pro.local>
2023-04-23 16:28:37 +0800

    getting transcript

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,43 +1,72 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const [name, setName] = React.useState('');
-  const [message, setMessage] = React.useState('');
+  const templist = [];//['testTranscript1.txt', 'testTranscript2.txt'];
+  const [blobsList, setblobsList] = React.useState(templist);
+  const [summary, setSummary] = React.useState('');
+  const [transcript, setTranscript] = React.useState('');
+  const [selectedItemId, setSelectedItemId] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
 
-  const getDataFromApi = async(e: any)=>{
-    e.preventDefault();
-    let url = "https://yashwisetestapp.azurewebsites.net/api/landing";
-    const data = await fetch(`${url}?name=${name}`);
-    console.log(data);
-    const json = await data.json();
-    if (json.message){
-      setMessage(json.message);
-    }
+  useEffect(() => {
+    const fetchData = async () => {
+      let url = "https://yashwisetestapp.azurewebsites.net/api/gettranscripts";
+      const data = await fetch(url);
+      console.log(data);
+      const json = await data.json();
+      if (json.blobs_list){
+        setblobsList(json.blobs_list);
+      }
+      setLoading(false)
+    };
+
+    fetchData();
+  }, []);
+
+  const handleItemClick = async(itemId, regenerate=false) => {
+    if (selectedItemId !== itemId || regenerate) {
+      setSelectedItemId(itemId);
+      setLoading(true)
+      let url = "https://yashwisetestapp.azurewebsites.net/api/httpexample";
+      const data = await fetch(`${url}?transcript=${blobsList[itemId]}`);
+      console.log(data);
+      const json = await data.json();
+      if (json.transcript) setTranscript(json.transcript);
+      if (json.summary) setSummary(json.summary);
+      setLoading(false)
+    } 
   };
+
+  const handleSalesForce = async() => {
+
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>{loading && <p>Abhi</p>}</div>
         <p>
-          Static Web App: React App with Azure Function API
+          Select Transcript to view Summary
         </p>
-        <form id="form1" className="App-form" onSubmit={e => getDataFromApi(e)}>
-          <div>
-            <input 
-              type="text" 
-              id="name" 
-              className="App-input" 
-              placeholder="Name" 
-              value={name} 
-              onChange={e=>setName(e.target.value)} />
-            <button type="submit" className="App-button">Submit</button>
-          </div>
-        </form>
-        <div><h5>Message: {message} </h5></div>
+        <ul>
+          {blobsList.map( (item,index) => (
+            <li key={index} 
+                onClick={() => handleItemClick(index, false)}
+                style={{ textDecoration: selectedItemId === index ? 'underline' : 'none' }}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+        <div><h5>Transcript:</h5></div>
+        <p>{transcript}</p>
+        <div><h5>Spiced Transcript:</h5></div>
+        <p>{summary}</p>
+        <button onClick={() => handleItemClick(selectedItemId, true)}>Regenerate</button>
+        <button onClick={() => handleSalesForce()}>Save to Salesforce</button>
       </header>
     </div>
   );
